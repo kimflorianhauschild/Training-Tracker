b6d40a105a6e9ede72bf6c8cfa25b9d878de4226
Kim Hauschild <kim.hauschild@dataciders.com>
2026-09-01 12:28:55 +0200

    Deploy app with GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/Training-Tracker/",
   plugins: [
     react(),
     VitePWA({
@@ -16,7 +17,7 @@ export default defineConfig({
         background_color: "#111827",
         display: "standalone",
         orientation: "portrait",
-        start_url: "/",
+        start_url: "./",
         icons: [
           {
             src: "favicon.svg",
